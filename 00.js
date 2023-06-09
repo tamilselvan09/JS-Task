f47cd5448de2f4a279ebbcbf22448fda7767c8c2
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-09 23:02:41 +0530

    assin

---
+++ 00.js
@@ -0,0 +1,8 @@
+var a = [10,20,30]
+var b = [10,20,30]
+
+console.log(a = b) //[10,20,30]
+console.log(a == b) //true
+console.log(a === b) //true
+console.log(a[0] === b[0]) //true
+console.log(a[0] == b[1]) //false
---
+++ 05.js
@@ -0,0 +1,16 @@
+// WAP to Print - 3 digit numer or not ?
+//100 to 999
+
+var a = 799;
+
+if (a >= 100 && a <=999){
+    console.log("Yes")
+}
+else{
+    console.log("Not")  //yes
+}
+
+//Ternay
+var b = 1000
+b >= 100 && b <=999 ? console.log("Yes") :   console.log("Not")
+  //Not 
